f27cd0384add092613791ed2817865a62ea394b9
Conspiciens <73730667+Conspiciens@users.noreply.github.com>
2021-01-11 02:19:03 -0800

    Add files via upload

--- discord_bot_Vidi.py
+++ discord_bot_Vidi.py
@@ -16,14 +16,6 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='-')
 
 
-def time_start():
-    return "Stock Market Opens"
-
-
-def time_stop():
-    return "Stock Market Closes"
-
-
 @bot.command(name='99')
 async def loser_loser(ctx):
     await ctx.send('Loser')
@@ -40,27 +32,38 @@ async def stock_tesla(ctx, arg):
 
     try:
         gain = info.find_all('span', class_='Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)')
-        print(gain[0])
+        gain[0] = str(gain[0])
+
+        if '<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)" data-reactid="51">' in gain[0]:
+            g = gain[0].replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)" data-reactid="51">', '')
+            x = g.replace('</span>', '')
+        else:
+            h = gain[0].replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)" data-reactid="34">', '')
+            x = h.replace('</span>', '')
 
     except IndexError:
         gain = info.find_all('span', class_='Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)')
+        gain[0] = str(gain[0])
+
+        if '<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="51">' in gain[0]:
+            e = gain[0].replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="51">', '')
+            x = e.replace('</span>', '')
+        else:
+            f = gain[0].replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="34">', '')
+            x = f.replace('</span>', '')
 
     try:
+
+        final_info[0] = str(final_info[0])
+
         # Price
-        s = str(final_info[0]).replace('<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="50">', '')
-        f = s.replace('<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="33">', '')
+        if '<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="50">' in final_info[0]:
+            s = final_info[0].replace('<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="50">', '')
+        else:
+            s = final_info[0].replace('<span class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)" data-reactid="33">', '')
 
-        n = f.replace('</span>', '')
+        n = s.replace('</span>', '')
         u = n.replace(',', '')
-
-        # Gain or Loss
-        e = str(gain[0]).replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="51">', '')
-        g = e.replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)" data-reactid="51">', '')
-
-        o = g.replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)" data-reactid="34">', '')
-        l = o.replace('<span class="Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)" data-reactid="34">', '')
-
-        x = l.replace('</span>', '')
 
         # Check whether it has a price
         if float(u) >= 0:
